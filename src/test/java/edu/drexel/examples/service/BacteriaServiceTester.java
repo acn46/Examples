4b3142fa61ab873c794d248f928466d58c02c0e1
user <user@users-mbp.hsd1.nj.comcast.net>
2019-05-11 22:15:48 -0400

    ExternalizedConfig

--- src/test/java/edu/drexel/examples/service/BacteriaServiceTester.java
+++ src/test/java/edu/drexel/examples/service/BacteriaServiceTester.java
@@ -1,10 +1,13 @@
 package edu.drexel.examples.service;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,17 +22,34 @@ public class BacteriaServiceTester {
 	@Autowired
 	private BacteriaService service;
 	
+	@Value("${input_path}")
+	private String inputDirectory;
+	
 	@Test
 	public void test() {
-		String filename = "/Users/user/Downloads/Reports/AE0003_pair01.fastq.out.clean.csv.hc.report.csv";
 		
-		//BacteriaService service = new BacteriaService();
-		List<Bacteria> list = service.parse(filename);
-		System.out.println("List size = " + list.size());
-		//service.load(list);
+		//String inputDirectory = "/Users/user/Downloads/Reports/";
 		
-		Bacteria bt1 = service.findByTaxId(32002);
-		System.out.println("bt1 = " + bt1);
+		System.out.println("inputDirectory = " + inputDirectory);
+		
+//		File[] files = new File(inputDirectory).listFiles();
+//		
+//		//String filename = "/Users/user/Downloads/Reports/AE0003_pair01.fastq.out.clean.csv.hc.report.csv";
+//		
+//		for (File file : files) {
+//		    if (file.isFile()) {
+//		    	String filename = file.getPath();
+//		    	System.out.println("Parsing file " + filename);
+//		    	//BacteriaService service = new BacteriaService();
+//				List<Bacteria> list = service.parse(filename);
+//				System.out.println("Loading " + filename + " , list size = " + list.size());
+//				service.load(list);
+//		    }
+//		}
+//		
+		//Bacteria bt1 = service.findByTaxId(32002);
+		//System.out.println("bt1 = " + bt1);
+		
 	}
 
 }
